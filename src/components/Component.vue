b8238c411c3567b48eda5ff47fb2887ebb4d02fd
unknown <akariaka@yandex.ru>
2025-12-21 22:39:39 +0600

    ref: dom

--- src/components/Component.vue
+++ src/components/Component.vue
@@ -1,12 +1,37 @@
 <template>
-  <h1>maps</h1>
-  <form action="">
-    <label for="name">название</label>
-    <input name="name" type="text">
-    <input type="submit" value="добавить">
-  </form>
-  <div id="map" style="width: 600px; height: 400px"></div>
+  <div class="wrapper">
+    <div class="app-box app-box_1">
+      <h1>maps</h1>
+      <form class="form" action="">
+        <label for="name">название</label>
+        <input name="name" type="text">
+        <input type="submit" value="добавить">
+      </form>
+    </div>
+    <div class="app-box">
+      <div id="map" style="width: 600px; height: 400px"></div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .app-box_1 {
+    margin-right: 50px;
+  }
+</style>
 
 <script>
   initMap();
@@ -16,22 +41,17 @@
     await ymaps3.ready;
 
     const {YMap, YMapDefaultSchemeLayer} = ymaps3;
-
-    // Иницилиазируем карту
     const map = new YMap(
-        // Передаём ссылку на HTMLElement контейнера
-        document.getElementById('map'),
-
-        // Передаём параметры инициализации карты
-        {
-            location: {
-                // Координаты центра карты
-                center: [37.588144, 55.733842],
-
-                // Уровень масштабирования
-                zoom: 10
-            }
+      // Передаём ссылку на HTMLElement контейнера
+      document.getElementById('map'),
+      // Передаём параметры инициализации карты
+      {
+        location: {
+            // Координаты центра карты
+          center: [37.588144, 55.733842],
+          zoom: 10
         }
+      }
     );
 
     // Добавляем слой для отображения схематической карты
---
+++ src/components/coords.ts
@@ -0,0 +1,6 @@
+import type {YMapLocationRequest} from '@yandex/ymaps3-types';
+
+export const LOCATION: YMapLocationRequest = {
+  center: [20.5092, 54.7081], // starting position [lng, lat]
+  zoom: 14.7 // starting zoom
+};
