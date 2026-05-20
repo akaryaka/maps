62a3de3c223fe3c373c546ebe921783172fc71c0
akariaka@yandex.ru <akariaka@yandex.ru>
2026-05-21 00:05:49 +0600

    ref structure project

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -37,27 +37,27 @@
 </style>
 
 <script>
-  initMap();
+  // initMap();
 
-  async function initMap() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
-    await ymaps3.ready;
+  // async function initMap() {
+  //   // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+  //   await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
-    const map: any = new YMap(
-      // Передаём ссылку на HTMLElement контейнера
-      document.getElementById('map'),
-      // Передаём параметры инициализации карты
-      {
-        location: {
-            // Координаты центра карты
-          center: [37.588144, 55.733842],
-          zoom: 10
-        }
-      }
-    );
+  //   const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+  //   const map: any = new YMap(
+  //     // Передаём ссылку на HTMLElement контейнера
+  //     document.getElementById('map'),
+  //     // Передаём параметры инициализации карты
+  //     {
+  //       location: {
+  //           // Координаты центра карты
+  //         center: [37.588144, 55.733842],
+  //         zoom: 10
+  //       }
+  //     }
+  //   );
 
-    // Добавляем слой для отображения схематической карты
-    map.addChild(new YMapDefaultSchemeLayer());
-  }
+  //   // Добавляем слой для отображения схематической карты
+  //   map.addChild(new YMapDefaultSchemeLayer());
+  // }
 </script>
