c4f07330b555a209db9cc41408a06d55bea03e46
akariaka@yandex.ru <akariaka@yandex.ru>
2026-05-21 00:47:33 +0600

    ref structure project; feat: add label in list

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,14 +1,48 @@
 <script lang="ts" setup>
+  import { reactive, ref } from 'vue';
+
+  interface Label {
+    label: string,
+    lat: number,
+    lang: number,
+  }
+
+  const mapLabels = reactive({
+    label: '',
+    lat: 0,
+    lang: 0,
+  });
+
+  const listLabels: any = ref([]);
+
+  const addLabels = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    const mapLabel: Label = {
+      label: mapLabels.label,
+      lat: mapLabels.lat,
+      lang: mapLabels.lang
+    }
+
+    listLabels.value.push(mapLabel);
+  }
 </script>
 
 <template>
   <div class="wrapper">
     <div class="app-box app-box_1">
       <h1>maps</h1>
-      <form class="form" action="">
-        <label for="name">название</label>
-        <input name="name" type="text">
-        <input type="submit" value="добавить">
+      <form class="form">
+        <input v-model="mapLabels.label" name="name" type="text" placeholder="название">
+        <input v-model="mapLabels.lat" name="name" type="text" placeholder="широта">
+        <input v-model="mapLabels.lang" name="name" type="text" placeholder="долгота">
+        <input type="submit" value="добавить" @click="addLabels">
+        <ul>
+          <li v-for="(item, index) in listLabels" :key="index">
+            <p>{{ item.label }}</p>
+            <p>{{ item.lat }}</p>
+            <p>{{ item.lang }}</p>
+          </li>
+        </ul>
       </form>
     </div>
     <div class="app-box">
@@ -35,29 +69,3 @@
     margin-right: 50px;
   }
 </style>
-
-<script>
-  // initMap();
-
-  // async function initMap() {
-  //   // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
-  //   await ymaps3.ready;
-
-  //   const {YMap, YMapDefaultSchemeLayer} = ymaps3;
-  //   const map: any = new YMap(
-  //     // Передаём ссылку на HTMLElement контейнера
-  //     document.getElementById('map'),
-  //     // Передаём параметры инициализации карты
-  //     {
-  //       location: {
-  //           // Координаты центра карты
-  //         center: [37.588144, 55.733842],
-  //         zoom: 10
-  //       }
-  //     }
-  //   );
-
-  //   // Добавляем слой для отображения схематической карты
-  //   map.addChild(new YMapDefaultSchemeLayer());
-  // }
-</script>
